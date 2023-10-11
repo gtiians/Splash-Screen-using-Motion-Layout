71b0796f1d45a5a54c2a7f7ed78baa5d8a95bf2f
gtiians <gtiians@yahoo.com>
2023-10-11 20:16:37 +0530

    commiting

---
+++ app/src/main/java/gtiians/twitter/splash/MainActivity.java
@@ -0,0 +1,33 @@
+package gtiians.twitter.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+            finish();
+        },3000);
+
+
+    }
+
+
+}
